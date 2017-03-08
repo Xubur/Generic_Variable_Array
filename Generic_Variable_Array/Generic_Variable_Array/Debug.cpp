5b8ee01593918e9409fc2bcc29ec858957ffa376
Xubur <CodyCodington1731@gmail.com>
2017-03-08 13:56:49 -0600

    Updates.

--- Generic_Variable_Array/Generic_Variable_Array/Debug.cpp
+++ Generic_Variable_Array/Generic_Variable_Array/Debug.cpp
@@ -3,37 +3,37 @@
 #include "genVarArray.h"
 
 using namespace std;
-void testfunc(genVarArray<char>);
+void testfunc(genVarArray<string>);
 
 int main() {
 
-	genVarArray<char> a1;
-	// testing regular member functions
-	a1.add('a');
-	a1.add('b');
-	a1.add('c');
-	a1.add('c'); // trying to add duplicate, should not add it
+	genVarArray<string> a1;
+
+	a1.add("a1-First");
+	a1.add("a1-Second");
+	a1.add("a1-Third");
+	a1.add("a1-Third"); // trying to add duplicate, should not add it
 
 	a1.output();
 
-	if (a1.check('a') == -1) // check() returns -1 if number not present
-		cout << "1 is not present in the array" << endl;
+	if (a1.check("a1-First") == -1) 
+		cout << "'First' is not present in the array" << endl;
 
-	if (a1.check('g') == -1)
-		cout << "g is not present in the array" << endl;
+	if (a1.check("CompSci") == -1)
+		cout << "'CompSci' is not present in the array" << endl;
 
 	a1.output();
-	a1.remove('b');
+	a1.remove("a1-Second");
 	a1.output();
 
-	genVarArray<char> a2, a3;
+	genVarArray<string> a2, a3;
 
-	a2.add('a');
-	a2.add('b');
+	a2.add("a2-One");
+	a2.add("a2-Two");
 
-	a3.add('b');
-	a3.add('c');
-	a3.add('a');
+	a3.add("a3-One");
+	a3.add("a3-Two");
+	a3.add("a3-Three");
 
 	a3 = a2 = a1; // testing overloaded assignment
 	std::cout << "a3 = a2 = a1: \n"; a3.output(); std::cout << std::endl;
@@ -41,14 +41,13 @@ int main() {
 	std::cout << "a3 = a3"; a3.output(); std::cout << std::endl;
 
 	testfunc(a3); // testing copy constructor
-	a3.output(); // if destructor is implemented correctly
-				 // this should print properly after testfunc completes
+	a3.output(); // if destructor is implemented correctly this should print properly after testfunc completes
 
 } // end of function main.
 
 
   // tests pass-by-value for object of class genVarArray<>
-void testfunc(genVarArray<char> va)
+void testfunc(genVarArray<string> va)
 { // copy constructor is invoked on "va"
 	va.output();
 }
